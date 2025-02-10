3e4217ffd48f915b25dda0ced5b18ef0c7fd6d32
Tabong-Ang <tabongphilip@gmail.com>
2025-02-10 12:08:56 +0100

    finished including scroll to page

--- script.js
+++ script.js
@@ -76,9 +76,107 @@ document.addEventListener("DOMContentLoaded", () => {
 
   ScrollReveal().reveal(".sample_meals .sample_1", {
     ...scrollRevealOption,
-    // delay: 1000,
+    delay: 500,
   });
 
+  ScrollReveal().reveal(".sample_meals .sample_1 button", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
 
+  ScrollReveal().reveal(".book_a_table h2", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".book_a_table p", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal(".book_a_table button", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal(".table_types img", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".food_gallery h3", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".food_gallery p", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal(".food_gallery food img", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal(".food_gallery button", {
+    ...scrollRevealOption,
+    delay: 1200,
+  });
+
+  ScrollReveal().reveal(".location h4", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".location p", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal(".location .pics_iframe_box .pics_box", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal(".location .pics_iframe_box iframe", {
+    ...scrollRevealOption,
+    delay: 1200,
+  });
+
+  ScrollReveal().reveal(".contact_hours_of_operation .hours_of_operation h5", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".contact_hours_of_operation .hours_of_operation p", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal(".contact_hours_of_operation contact_form", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal(".contact_hours_of_operation contact_form button", {
+    ...scrollRevealOption,
+    delay: 1200,
+  });
+
+  ScrollReveal().reveal("footer .bg-colo h6", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal("footer .bg-colo p", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal("footer .bg-colo address", {
+    ...scrollRevealOption,
+    delay: 1200,
+  });
+
+  ScrollReveal().reveal("footer .bg-colo label", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
 
 });
